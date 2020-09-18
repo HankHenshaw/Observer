934ed1a96eedc68e31b5096d2800ae040fee3e26
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-18 12:45:13 +0000

    Deploy HankHenshaw/Observer to github.com/HankHenshaw/Observer.git:gh-pages

---
+++ search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "cfimors",
+  1: "cfis",
+  2: "mo",
+  3: "r"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "Указатель",
+  1: "Классы",
+  2: "Файлы",
+  3: "Страницы"
+};
+
